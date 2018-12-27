d5aa290c5f38906459a6e2ebcc20af3e43c8b033
Pedro Torchio <pedrotorchio@gmail.com>
2018-12-27 17:22:14 -0300

    nav

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,37 @@
 <template lang="pug">
   div#app
-    div#nav.section.full-height
-      router-link( to="/" ) Sobre
-      router-link( to="/servicos" ) Serviços
-      router-link( to="/localizacao" ) Localização
-      router-link( to="/contato" ) Contato
+    div#nav-container
+      nav#main.section
+        router-link( to="/" ) Sobre
+        router-link( to="/servicos" ) Serviços
+        router-link( to="/localizacao" ) Localização
+        router-link( to="/contato" ) Contato
 
-    router-view
+    router-view#view
 
 </template>
 
+<style lang="sass" scoped>
+$nav-height: 30px
+
+#nav-container
+  position: absolute
+  height: 1.2em
+  top: calc(100vh - 1.2em)
+  font-size: $nav-height
+
+  a
+    margin: 0 .2em
+
+#view
+  max-width: 1600px
+  width: 100%
+  margin: 0 auto
+</style>
+
 <style lang="sass">
+a
+  text-decoration: none
 body
   margin: 0
 
