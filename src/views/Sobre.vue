80e5122a3d1a31e75c550428e5f622cc22ce9a15
Pedro Torchio <pedrotorchio@gmail.com>
2018-12-28 00:18:54 -0300

    image blurring

--- src/views/Sobre.vue
+++ src/views/Sobre.vue
@@ -1,12 +1,22 @@
 <script>
 import lazyImage from 'v-lazy-image';
+
+import header from './sobre/header';
+
 export default {
   name: 'home',
+  mixins: [ header ],
   components: { lazyImage }
 }
 </script>
 
 <template lang="pug">
   div#sobre
-    lazy-image( src = "/assets/imgs/about-hero.jpg" )
+    header.full-height.section
+      lazy-image.cover(
+        :class = "{ 'not-loaded': !isHeaderImageLoaded }"
+        src = "/assets/imgs/about-hero.jpg"
+        src-placeholder = "/assets/imgs/about-hero-tiny.jpg"
+        @load = "headerImageLoaded" )
 </template>
+
