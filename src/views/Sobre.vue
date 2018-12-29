cf7b652af79a44442b2caca87562fa56d2aada78
Pedro Torchio <pedrotorchio@gmail.com>
2018-12-28 21:47:37 -0300

    line animation (still not positioned right)

--- src/views/Sobre.vue
+++ src/views/Sobre.vue
@@ -1,6 +1,6 @@
 <script>
 import lazyImage from 'v-lazy-image';
-
+import '@/components/svg/about-hero-line';
 import header from './sobre/header';
 
 export default {
@@ -18,9 +18,22 @@ export default {
         src = "/assets/imgs/about-hero.jpg"
         src-placeholder = "/assets/imgs/about-hero-tiny.jpg"
         @load = "headerImageLoaded" )
+      
+      transition( appear @before-enter = "beforeLineEnter" @enter = "lineEnter" )
+        svgicon#header-line( v-if = "isHeaderImageLoaded" name = "about-hero-line" :original = "true" )
 </template>
 
 <style lang="sass" scoped>
 header img
-  object-position: left
+  object-position: 35% center
+
+#header-line
+  position: absolute;
+  z-index: 555;
+  left: 0;
+  top: 0
+  width: 100%;
+  height: 100%;
+
+    
 </style>
--- src/views/sobre/header.vue
+++ src/views/sobre/header.vue
@@ -1,4 +1,5 @@
 <script>
+import { TweenMax, SlowMo } from 'gsap';
 export default {
     data: () => ({
         isHeaderImageLoaded: false
@@ -6,6 +7,22 @@ export default {
     methods: {
         headerImageLoaded() {
             this.isHeaderImageLoaded = true;
+        },
+        beforeLineEnter(el, done) {
+            const path = el.querySelector('path');
+            const length = path.getTotalLength();
+            
+            path.style.strokeDasharray = length;
+            path.style.strokeDashoffset = length;
+
+        },
+        lineEnter(el, done) {
+
+            TweenMax.to(el.querySelector('path'), 1, {
+                strokeDashoffset: 0,
+                ease: SlowMo.ease.config(0.5, 0.7, false),
+                delay: 4
+            });
         }
     }
 }
