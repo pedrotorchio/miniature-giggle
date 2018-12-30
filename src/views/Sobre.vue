15ac9104b420f69a55479d6549fbb0d0ec297ef4
Pedro Torchio <pedrotorchio@gmail.com>
2018-12-30 20:31:25 -0300

    cta border animation

--- src/views/Sobre.vue
+++ src/views/Sobre.vue
@@ -1,7 +1,7 @@
 <script>
 import lazyImage from 'v-lazy-image';
 import '@/components/svg/about-hero-line';
-import Header from './sobre/Header.mixin';
+import Header from './sobre/Header.section';
 import Route from '@/mixins/Route.mixin';
 export default {
   name: 'home',
@@ -21,10 +21,21 @@ export default {
         @load = "headerImageLoaded" )
       
       transition( appear @before-enter = "beforeLineEnter" @enter = "lineEnter" )
-        svgicon#header-line( v-if = "isHeaderImageLoaded" name = "about-hero-line" :original = "true" )
+        svgicon#header-line( v-if = "isHeaderImageLoaded" name = "about-hero-line" :fill = "false" color = "#7dc4c7" )
+      
+      h2#cta( ref = "cta" )
+        span.top( ref = "ctaBorderTop" )
+        span.bottom( ref = "ctaBorderBottom" )
+        span.left( ref = "ctaBorderLeft" )
+        span.right( ref = "ctaBorderRight" )
+
+        span.text( ref = "ctaLetters" v-for = "( letter, i ) in ctaTextArray" :key = "letter + i" :data-index = "i" ) {{ letter }}
+
 </template>
 
 <style lang="sass" scoped>
+@import '~@/styles/config'
+
 header img
   object-position: 35% center
 
@@ -37,4 +48,49 @@ header img
   height: 100%;
 
     
+#cta
+  will-change: transform
+  position: absolute;
+  bottom: 100px;
+  right: 66px;
+  color: $color--primary;
+  text-transform: uppercase;
+  letter-spacing: .1em;
+  border: 0 solid;
+  padding: 1em;
+  border-radius: 0;
+  cursor: pointer
+  visibility: hidden
+  z-index: 5555
+  
+  .top, .right, .left, .bottom
+    position: absolute
+    background-color: $color--primary
+    transition-property: height, width
+    transition-duration: 1s
+  
+  .top, .bottom
+    width: 0%
+    height: 5px;
+    &.shown
+      width: 100%
+  .left, .right
+    height: 0%
+    width: 5px
+    &.shown
+      height: 100%
+
+  .top, .right
+    top: 0
+  .top, .left
+    left: 0
+  
+  .bottom, .left
+    bottom: 0
+  .bottom, .right
+    right: 0
+
+  span.text
+    visibility: hidden;
+    will-change: transform
 </style>
