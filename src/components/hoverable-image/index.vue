0ef936e7d577119d32606ec59fcbdee4cf18c4f1
Pedro Torchio <pedrotorchio@gmail.com>
2019-01-02 18:24:51 -0300

    hoverable image invertable

--- src/components/hoverable-image/index.vue
+++ src/components/hoverable-image/index.vue
@@ -7,6 +7,10 @@ export default {
         hoverable: {
             type: Boolean,
             default: true
+        },
+        invert: {
+            type: Boolean,
+            default: false
         }
     },
     computed: {
@@ -22,50 +26,76 @@ export default {
 }
 </script>
 <template lang="pug">
-    div.hoverable-image( :class = "{ hoverable }")
+    div.hoverable-image( :class = "[ invert ? 'invert' : 'regular', { hoverable }]")
         lazy-image(
             v-bind = "attrs"
             v-on = "listeners"
         )
-        slot.caption
+        div.caption
+            slot
 </template>
 <style lang="sass" scoped>
-    @mixin transition( $property )
+    =transition( $property )
         transition-property: $property
         transition-duration: .5s
         transition-timing-function: cubic-bezier(0.23, 1, 0.32, 1)
+
+    %cover
+        position: absolute
+        width: 100%
+        height: 100%
+        top: 0
+        bottom: 0
+        left: 0
+        right: 0
+        object-fit: cover
+        object-position: center center
 
     .hoverable-image
         overflow: hidden
         position: relative
 
-        &.hoverable
-            &:before
-                content: ''
-                width: 100%
-                height: 100%
-                position: absolute
-                opacity: 0
-                background-color: black
-                z-index: 55
-                +transition(opacity)
-    img
-        +transition((transform, filter))
-        height: 100%
-        width: 100%
-        object-fit: cover
-        z-index: 5
-        object-position: center center
-    .caption
-        visibility: hidden
+        *
+            cursor: initial
+        &:before
+            @extend %cover
+            content: ''
+            background-color: black
+            z-index: 55
+            +transition(opacity)
 
-    .hoverable-image.hoverable:hover
+    img
+        @extend %cover
+        +transition((transform, filter))
+        z-index: 5
+    .caption
+        @extend %cover
+        z-index: 555
+    
+    
+    %with-overlay
         img
             transform: scale(1.2)
-            filter: blur(5px) grayscale(50%)
+            filter: blur(3px) grayscale(50%)
         &:before
-            opacity: .5
+            opacity: .5        
         .caption
             visibility: visible
+    %without-overlay
+        img
+            transform: scale(1)
+            filter: blur(0px) grayscale(20%)
+        &:before
+            opacity: 0
+    
+    .hoverable-image.regular
+        @extend %without-overlay
+    .hoverable-image.regular.hoverable:hover
+        @extend %with-overlay
+
+    .hoverable-image.invert
+        @extend %with-overlay
+    .hoverable-image.invert.hoverable:hover
+        @extend %without-overlay
 </style>
 
