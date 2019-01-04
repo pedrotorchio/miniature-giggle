b6578b793e2e38c6a05abc109ccb4187b07fd273
Pedro Torchio <pedrotorchio@gmail.com>
2019-01-04 18:59:00 -0300

    reato svg generate

---
+++ src/components/svg/reato.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* tslint:disable */
+// @ts-ignore
+import icon from 'vue-svgicon'
+icon.register({
+  'reato': {
+    width: 16,
+    height: 16,
+    viewBox: '0 0 320.17 84',
+    data: '<g id="reato"><path pid="0" d="M378.78 104.86a24.39 24.39 0 0 0-24.3-24.4h-22.06v78.08h9.76v-29.32h10.45l19.23 29.28h11.71l-20.4-30.94a24.33 24.33 0 0 0 15.61-22.7zm-36.6 14.64V90.28h12.3a14.6 14.6 0 0 1-.1 29.19h-12.2z" transform="translate(-332.42 -77.49)" _fill="#1b3c6c" id="reato-r"/><path pid="1" _fill="#1b3c6c" d="M71.16 46.85h27.63v-9.76H71.16v-24.4h32.51V2.93H61.4v78.08h42.46v-9.76h-32.7v-24.4z" id="reato-e"/><path pid="2" d="M446.12 158.5h10.74l7.51-15.13H506l7.42 15.13h10.74l-38.65-81zm22.64-24.89l16.69-34 16.21 34h-32.9z" transform="translate(-332.42 -77.49)" _fill="#1b3c6c" id="reato-a"/><path pid="3" _fill="#1b3c6c" d="M188.78 12.69h17.37v68.32h9.76V12.69h17.38V2.93h-44.51v9.76z" id="reato-t"/><path pid="4" d="M610.59 77.49a42 42 0 1 0 42 42 42 42 0 0 0-42-42zm0 74.28a32.31 32.31 0 1 1 32.31-32.31 32.4 32.4 0 0 1-32.31 32.31z" transform="translate(-332.42 -77.49)" _fill="#1b3c6c" id="reato-o"/></g>'
+  }
+})
