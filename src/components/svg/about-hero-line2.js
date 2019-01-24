87807a24b13c15d927eaf2c8a2ec26ca34520e51
Pedro Torchio <pedrotorchio@gmail.com>
2019-01-23 23:59:22 -0300

    including line svg

---
+++ src/components/svg/about-hero-line2.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* tslint:disable */
+// @ts-ignore
+import icon from 'vue-svgicon'
+icon.register({
+  'about-hero-line2': {
+    width: 16,
+    height: 16,
+    viewBox: '0 0 7233.97 2362.73',
+    data: '<path pid="0" id="line" d="M7220.98 21.36l-1552 944-1920 680-1016-512s-768 328-816 328-376-48-376-48l-1528 928" _fill="none" _stroke="#000" stroke-miterlimit="10" stroke-width="50"/>'
+  }
+})
--- src/components/svg/index.js
+++ src/components/svg/index.js
@@ -1,5 +1,6 @@
 /* eslint-disable */
 import './about-hero-line'
+import './about-hero-line2'
 import './logo-full'
 import './logo-head'
 import './reato'
